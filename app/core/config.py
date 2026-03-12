bd021c70846c064898f07097497c8f2970b7bdb9
AS1 <arseniykofanov@gmail.com>
2026-03-12 16:51:01 +0300

    Храним в env данные для входа, а не ссылку. При инициализации класса настроек создаём переменную для удобства.

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -9,12 +10,17 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
-    database_url: str = Field(
-        "postgresql+asyncpg://postgres:postgres@db:5432/postgres_typo",
-        validation_alias="DATABSE_URL",
-    )
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str 
+
+    database_url: Optional[str] = None
+
     log_level: str = "INFO"
     parse_schedule_minutes: int = 5
+
+    def model_post_init(self, __context):
+        self.database_url = f'postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@db:5432/{self.postgres_db}'
 
 
 settings = Settings()
